117ed7b29eb8fdfa676ad31e3fec418b664d4c9f
hornadam8 <hornadam8@gmail.com>
2020-06-02 10:47:28 -0600

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    def create
+        if params[:user][:password] == params[:user][:password_confirmation]
+            @user = User.create(user_params)
+            session[:user_id] = @user.id
+            redirect_to '/users/home'
+        else
+            redirect_to '/users/new'
+        end
+    end
+    
+    def home
+        @user = current_user
+    end
+
+    private
+
+        def current_user
+            User.find(session[:user_id])
+        end
+
+        def user_params
+            params.require(:user).permit(:name,:password,:password_confirmation)
+        end
+
+
+end
