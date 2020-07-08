c8224141fbd595538b81bed7e0070bd878fd711b
848smith <848smith@gmail.com>
2020-07-08 02:06:58 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def new
+
+    end
+    
+    def create
+        @user = User.create(user_params)
+        if params[:user][:password] != params[:user][:password_confirmation]
+            redirect_to '/users/new'
+        else
+            session[:user_id] = @user.id
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
